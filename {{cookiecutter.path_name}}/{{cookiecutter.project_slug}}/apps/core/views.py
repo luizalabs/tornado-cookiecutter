4ca032dde42f13bc56be341ea683306e9ef4f73e
Rodrigo Savian <rodrigo.savian@luizalabs.com>
2016-05-19 09:28:04 -0300

    Adding docs scructure on project.

--- {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/apps/core/views.py
+++ {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/apps/core/views.py
@@ -5,3 +5,11 @@ class HomeHandler(tornado.web.RequestHandler):
 
     def get(self):
         self.write("Hello world")
+
+
+class DocsHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        version = "{}://{}/docs/version/v1.yml".format(self.request.protocol,
+                                                       self.request.host)
+        self.render("swagger/index.html", version=version)
--- {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/apps/urls.py
+++ {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/apps/urls.py
@@ -6,10 +6,14 @@ from contrib.urls import include
 from settings import settings
 
 from apps.core.views import HomeHandler
+from apps.core.views import DocsHandler
 
 
 urls = [
     url(r"/", HomeHandler),
+    url(r"/docs", DocsHandler),
+    url(r"/docs/version/(.*)", web.StaticFileHandler,
+        {"path": settings.DOCS_ROOT}),
     url(r"/static/(.*)", web.StaticFileHandler,
         {"path": settings.STATIC_ROOT})
 ]
