e103f0cd84d6e47d6b6902a18014e2071288d97b
Matheus Oliveira <oliveira.matheusde@gmail.com>
2016-05-20 02:48:50 -0300

    Refactoring customers. Create a more scene test

--- {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/apps/customers/tests/test_api.py
+++ {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/apps/customers/tests/test_api.py
@@ -1,16 +1,24 @@
 import pytest
 
-from mixer.backend.sqlalchemy import mixer
-
 from apps.customers.models import Customer
 
 
 @pytest.fixture
-def customers_mock_data():
+def customers_mock_data(mixer):
     mixer.cycle(4).blend(Customer)
 
 
 @pytest.mark.gen_test
-def test_list(db, customers_mock_data, http_client, base_url):
+def test_customers_list(customers_mock_data, http_client, base_url):
     resp = yield http_client.fetch('{}/{}'.format(base_url, 'customers'))
+
+    assert resp.code == 200
+
+
+@pytest.mark.gen_test
+def test_customers_detail(customers_mock_data, http_client, base_url):
+    resp = yield http_client.fetch(
+        '{}/{}/{}'.format(base_url, 'customers', 1)
+    )
+
     assert resp.code == 200
