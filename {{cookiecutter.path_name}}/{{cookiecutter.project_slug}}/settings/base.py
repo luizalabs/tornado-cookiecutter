c0ae380339c9147d0ac165c358bf5c22440b56ce
Matheus Oliveira <oliveira.matheusde@gmail.com>
2016-05-20 02:43:14 -0300

    Change database name to load a env var

--- {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/settings/base.py
@@ -14,7 +14,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'sqlite',
         'HOST': os.getenv('DEFAULT_HOST'),
-        'NAME': 'default.db',
+        'NAME': os.getenv('DEFAULT_NAME'),
         'USER': os.getenv('DEFAULT_USER'),
         'PASSWORD': os.getenv('DEFAULT_PASSWORD'),
         'PORT': os.getenv('DEFAULT_PORT')
