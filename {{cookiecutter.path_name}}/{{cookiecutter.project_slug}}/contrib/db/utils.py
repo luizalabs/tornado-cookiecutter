ca8762c3e7c6f8fb1b9fd03e7ad7eda1a76c6806
Matheus Oliveira <oliveira.matheusde@gmail.com>
2016-05-25 09:21:31 -0300

    Add comments and return a bind arg

--- {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/contrib/db/utils.py
+++ {{cookiecutter.path_name}}/{{cookiecutter.project_slug}}/contrib/db/utils.py
@@ -3,8 +3,34 @@ from restless.exceptions import NotFound
 from .session import session
 
 
-def get_or_404(_model, id):
-    obj = session.query(_model).get(id)
+def get_or_404(_model, id, bind=None):
+    """
+    Return object or not found exception
+
+    :param _model: Model
+    :param id: Primary key
+    :param bind: Name of other engine
+
+    Example:
+
+        # without bind
+        food = get_or_404(Food, 1)
+        food.name
+        >>> Espinafre
+
+        # with bind
+        food = get_or_404(Food, 1, 'other1')
+        food.name
+        >>> CuzCuz
+
+        # not found
+        food = get_or_404(Food, 42)
+        # { "error": "Food not found" }
+    """
+    if bind:
+         obj = session.using(bind).query(_model).get(id)
+    else:
+        obj = session.query(_model).get(id)
 
     if obj:
         return obj
